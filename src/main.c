1ba8f092242566ba486c047bf8bc6b0797e56b93
llxx <lixin65535@126.com>
2015-12-03 10:16:52 +0800

    #

--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-//unix 
+//unix
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -14,13 +14,16 @@
 #include <signal.h>
 //data structs
 #include "HString.h"
+//sign
+#include <openssl/evp.h>
+#include <b64/base64.h>
 
 #define IP "192.168.3.17"
 #define PORT 10002
 
 #define BS 1024   ///buff size
 
-#define METHOD_GET 1 
+#define METHOD_GET 1
 #define METHOD_POST 2
 
 #define WWW_ROOT "/home/public/htdocs"
@@ -31,14 +34,13 @@ void response(int);
 void sigfunc_callback(int sig);
 void sig_chld_func();
 void sig_int_func();
+char* keygen(const char *key);
 
 typedef struct {
-    int method;
-    char file[1024];
-    char host[1024];
+    char accept_key[1024];
 } RequestInfo;
 
-RequestInfo req_info = {0,"\0","\0"} ;
+RequestInfo req_info = {"\0"} ;
 
 int serv_sock_f ;
 
@@ -58,16 +60,16 @@ int main()
 
 
     int flag=1 ;
-    
+
     // SO_REUSEADDR 设置端口重用，否则中断程序重启会提示端口被使用，需要过一段时间才能使用
-    int len=sizeof(int); 
+    int len=sizeof(int);
     if( setsockopt(serv_sock_f, SOL_SOCKET, SO_REUSEADDR, &flag, len) == -1)
-    { 
-       perror("setsockopt"); 
-       exit( -1 ); 
-    } 
+    {
+       perror("setsockopt");
+       exit( -1 );
+    }
     //发现设置端口重用有个问题，如果中断了该端口通信（通信正在进行中），重用的端口的新程序会继续接收上次未完成的数据（然后就杯具了%>_<%）
-    
+
 	flag = bind(serv_sock_f, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
 
 
@@ -84,22 +86,30 @@ int main()
 		exit( -1 );
 	}
 
-	
+
     struct sockaddr_in client_addr;
 	socklen_t client_sock_l = 0;
 	int client_sock_f = -1;
 
+    while(1){
+
     // >>>>>>>>>> test
-    /*
     client_sock_f = accept(serv_sock_f, (struct sockaddr *)&client_addr, &client_sock_l);
+    //for login
     read_header(client_sock_f);
     response(client_sock_f);
-    close(client_sock_f) ;
-    close(serv_sock_f);
-    */
+    //login done  begin msg ...
+
+
+    }
+
+    //disconnection
+    //close(client_sock_f) ;
+    //close(serv_sock_f);
     // >>>>>>>>>> test end
 
-    
+    /*
+
     while(1){
         client_sock_f = accept(serv_sock_f, (struct sockaddr *)&client_addr, &client_sock_l);
         pid_t pid = fork();
@@ -115,8 +125,9 @@ int main()
             break;
         }
     }
-    
-    
+    */
+
+
     exit(0);
 }
 
@@ -160,94 +171,92 @@ void sig_chld_func()
               printf("unknow \n");
 
      }
-    
+
 }
 
 void response(int client_sock_f){
 
-    if(req_info.method<=0 || strlen(req_info.file)<=0 || strlen(req_info.host)<=0){
-        return ;
-    }
+    char *format = "HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: %s\r\n\r\n\0" ;
 
-    char *_html = "<h1>Hello C</h1>";
-    int html_l = strlen(_html);
-
-    int html_buf_size = html_l + BS;
-
-    char *format = "HTTP/1.1 200 OK\r\nContent-Length:%d\r\n\r\n%s" ;
-    char *html = (char *)malloc(html_buf_size);
-    strncpy(html, _html, html_l);
-
-    if(strlen(req_info.file)>1){
-
-        char realpath[1024];
-        sprintf(realpath, "%s%s", WWW_ROOT, req_info.file);
-        printf("will read content from %s \n", realpath);
-        FILE *fp = fopen(realpath, "r");
-        if(fp==NULL){
-            format = "HTTP/1.1 404 Not Found\r\nContent-Length:%d\r\n\r\n%s" ;
-            _html = "<h1>Object Not Found !</h1>";
-            html_l = strlen(_html) ;
-            html = (char *)realloc(html, html_l+BS);
-            strncpy(html, _html, html_l);
-
-        }else{
-
-            memset(html, '\0', html_buf_size);
-            html_l = 0;
-
-            int rflen = 0 ;
-            while(!feof(fp)){
-                if(html_buf_size <= html_l+BS) {
-                    html_buf_size += BS+1;
-                    html = (char *)realloc(html, html_buf_size);
-                }
-                rflen = fread(html+html_l, sizeof(char), BS, fp);
-                html_l += rflen;
-            }
-
-        }
-    }
-    
-    *(html+html_l) = '\0' ;
-    
     int format_l = strlen(format);
+    size_t key_len = strlen(req_info.accept_key);
 
-    size_t sss = html_l+format_l+100 ;
-    char *response_buf = (char *)malloc( sss );
-    memset(response_buf, '\0', sss);
-    
+    size_t _response_l = key_len+format_l+100 ;
+    char *response_buf = (char *)malloc( _response_l );
+    memset(response_buf, '\0', _response_l);
+
     // 组装响应数据
-    sprintf(response_buf, format, html_l, html);
+    sprintf(response_buf, format, req_info.accept_key);
     printf("%s \n", response_buf);
 
     int __l = send(client_sock_f, response_buf, strlen(response_buf), 0);
 
 
     if(__l<0){
-    
+
         perror("send") ;
         exit(-1) ;
     }
 
-    printf("send %d \n", __l);    
+    printf("send %d \n", __l);
 }
 
 void parse_line(const char *str)
 {
-    if(strncmp("GET", str, 3)==0){
-        req_info.method = METHOD_GET ;
-        char _m[16];
-        char _f[1024];
-        char _v[32];
-        sscanf(str, "%s %s %s", _m, _f, _v) ;
-        strncpy(req_info.file, _f, strlen(_f));
-    }else if(strncmp("Host", str, 4)==0){
-        char _k[16];
+    if(strncmp("Sec-WebSocket-Key", str, 17)==0){
+        //req_info.method = METHOD_GET ;
+        char _k[32];
         char _v[1024];
-        sscanf(str, "%s %s", _k, _v);
-        strncpy(req_info.host, _v, strlen(_v));
+
+        sscanf(str, "%s %s", _k, _v) ;
+        //strncpy(req_info.file, _f, strlen(_f));
+        keygen(_v);
     }
+}
+
+char* keygen(const char *key)
+{
+    char *_k = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+    size_t _f_t = strlen(key) + strlen(_k) + 1 ;
+    char *mess = (char *)malloc( _f_t ) ;
+    memset(mess, '\0', _f_t);
+
+    sprintf(mess, "%s%s", key, _k);
+    // 将 SHA-1 加密后的字符串再进行一次 base64 加密
+    EVP_MD_CTX *mdctx;
+	const EVP_MD *md;
+	unsigned char md_value[EVP_MAX_MD_SIZE];
+	int md_len;
+
+	OpenSSL_add_all_digests();
+
+	md = EVP_get_digestbyname("sha1");
+
+	mdctx = EVP_MD_CTX_create();
+	EVP_DigestInit_ex(mdctx, md, NULL);
+	EVP_DigestUpdate(mdctx, mess, strlen(mess));
+	EVP_DigestFinal_ex(mdctx, md_value, &md_len);
+	EVP_MD_CTX_destroy(mdctx);
+
+	/* Call this once before exit. */
+	EVP_cleanup();
+
+    char *result = (char *)malloc(md_len+1);
+    memset(result, '\0', md_len+1);
+    memcpy(result, md_value, md_len);
+
+    // base64
+    // gcc -I./ -L/usr/lib64/openssl/engines/ -lcapi -o demo demo.c base64.c cencode.c cdecode.c
+    char* encoded = base64_encode(result);
+    printf("Sec-WebSocket-Accept: %s", encoded); /* encoded data has a trailing newline */
+    // encoded: Ck1VqNd45QIvq3AZd8XYQLvEhtD/fw==
+    free(mess);
+    free(result);
+
+    memcpy(req_info.accept_key, encoded, strlen(encoded));
+    free(encoded);
+
+    return "Ok";
 }
 
 void read_header(int cfd)
@@ -263,7 +272,7 @@ void read_header(int cfd)
     FILE *fp = fdopen(cfd, "r") ;
 
     HString hs = {NULL, 0};
-    
+
     buf = (char *)malloc(buf_size+1);
 
     while(1){
@@ -277,7 +286,7 @@ void read_header(int cfd)
             if(buf[_rlen-1]=='\n')
                 break;
         }
-        
+
         ToString(&hs, &line);
 
         if(strncmp("\r\n",line,2)==0){
@@ -287,11 +296,11 @@ void read_header(int cfd)
 
         if(strlen(line)<=0)
             break;
-        
+
         printf("%s", line);
         parse_line(line);
     }
 
-    printf("the request info is %d,%s,%s\n", req_info.method, req_info.file, req_info.host);
+    printf("the request info is %s\n", req_info.accept_key);
 
 }
